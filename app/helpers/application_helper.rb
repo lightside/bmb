8db8abb86b21b89f8fe4a1a8b63b229f15abc8c1
Ryan Johanningmeier <ryan@onlylightside.com>
2011-07-08 12:51:02 -0500

    Some DRY and link checking complete

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,9 @@
 module ApplicationHelper
 
+	def logo
+		logo = image_tag("lg_logo.gif", :alt => "Black Market Books", :class => "main_logo")
+	end
+
 	# Return a title on a per-page basis
 	def title
 		base_title = "Black Market Books"
--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -34,6 +34,24 @@ describe "LayoutLinks" do
 	
 	it "should have a Sign Up page at '/signup'" do
 		get '/signup'
-		response.should have_selector('title', :content => "Sign up")
+		response.should have_selector('title', :content => "Sign Up")
+	end
+	
+	it "should have the right links on the layout" do
+		visit root_path
+		click_link "Search"
+		response.should have_selector('title', :content => "Search")
+		click_link "FAQ"
+		response.should have_selector('title', :content => "FAQ")
+		click_link "Contact"
+		response.should have_selector('title', :content => "Contact")
+		click_link "Account"
+		response.should have_selector('title', :content => "Sign Up")
+		click_link "Terms of Use"
+		response.should have_selector('title', :content => "Terms of Use")
+		click_link "Privacy Policy"
+		response.should have_selector('title', :content => "Privacy Policy")
+		click_link "Home"
+		response.should have_selector('title', :content => "")
 	end
 end
