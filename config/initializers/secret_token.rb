22f5a3c43d22315e98233d4bf6446dfa481875d4
Ryan Johanningmeier <ryan@onlylightside.com>
2011-07-07 23:56:23 -0500

    Application install and database setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bmb::Application.config.secret_token = '4c82ed6e2014ee26c70c350cac65c9d329fc661bd3f7e3ff06063dcad5ad1e9f7ce0304af3005e664599200b911140c389198dd3e89061e010f010cb8be65ac8'
